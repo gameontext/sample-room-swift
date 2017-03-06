269735b957144bbf99d412ee38e8e642110f6b5e
Ozzy Osborne <bardweller@gmail.com>
2017-03-06 14:26:57 -0500

    Update Game On URL (#4)
    Signed-off-by: Ozzy Osborne <ozzy@ca.ibm.com>

--- Sources/SwiftRoom/RoomEndpoint.swift
+++ Sources/SwiftRoom/RoomEndpoint.swift
@@ -20,38 +20,38 @@ import KituraWebSocket
 /**
  * This is the WebSocket endpoint for a room. An instance of this class
  * will be created for every connected client.
- * https://book.game-on.org/microservices/WebSocketProtocol.html
+ * https://book.gameontext.org/microservices/WebSocketProtocol.html
  */
 
 public class RoomEndpoint: WebSocketService {
-    
+
     private let roomImplementation: RoomImplementation = RoomImplementation()
-    
+
     private var connections = [String: WebSocketConnection]()
-    
+
     public init() {}
-    
+
     public func connected(connection: WebSocketConnection) {
-        
+
         Log.info("A new connection has been made to the room.")
-        
+
         connections[connection.id] = connection
-        
+
         connection.send(message: Message.createAckMessage())
-        
+
     }
-    
+
     public func disconnected(connection: WebSocketConnection, reason: WebSocketCloseReasonCode) {
-        
+
         connections.removeValue(forKey: connection.id)
         Log.info("A connection to the room has been closed with reason \(reason.code())")
-        
+
     }
-    
+
     public func received(message: String, from: WebSocketConnection) {
         print("server received message: \(message)")
         for (_, connection) in connections {
-            
+
             do {
                 try roomImplementation.handleMessage(messageStr: message, endpoint: self, connection: connection)
             }
@@ -60,15 +60,15 @@ public class RoomEndpoint: WebSocketService {
             }
         }
     }
-    
+
     public func received(message: Data, from: WebSocketConnection) {
         from.close(reason: .invalidDataType, description: "GameOn Swift room only accepts text messages")
         connections.removeValue(forKey: from.id)
     }
-    
+
     public func sendMessage(connection: WebSocketConnection, message: Message) {
         print("server sending processed message to client: \(message.toString())")
         connection.send(message: message.toString())
-        
+
     }
 }
